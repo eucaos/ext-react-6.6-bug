3d776a6899d8add9a3529dce5c1dcf8545e4d346
eucaos <eucaos@yahoo.com>
2018-09-04 16:32:02 +0300

    Delete tst.bat

--- tst.bat
+++
@@ -1,6 +0,0 @@
-echo "# ext-react-6.6-bug" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/eucaos/ext-react-6.6-bug.git
-git push -u origin master
